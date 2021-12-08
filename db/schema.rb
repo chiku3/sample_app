84f522017503797685afb42f6178aec8f9c8065c
chiku3 <p314chiku.y@gmail.com>
2021-12-08 14:41:56 +0000

    Commit 12/8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_042938) do
+ActiveRecord::Schema.define(version: 2021_12_08_141047) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
