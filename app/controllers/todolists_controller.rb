9fab0def574c4fc6104c01e99031e80fef96a26b
chiku3 <p314chiku.y@gmail.com>
2021-12-09 05:54:51 +0000

    12/9-1

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,9 +4,12 @@ class TodolistsController < ApplicationController
   end
   
   def create
-   list = List.new(list_params)
-   list.save
-   redirect_to todolist_path(list.id)
+   @list = List.new(list_params)
+   if @list.save
+    redirect_to todolist_path(@list.id)
+   else
+    render:new
+   end
   end
   
   def index
@@ -26,6 +29,9 @@ class TodolistsController < ApplicationController
       list.update(list_params)
       redirect_to todolist_path(list.id)
   end
+  
+  def destroy
+ end
   
   private
   def list_params
